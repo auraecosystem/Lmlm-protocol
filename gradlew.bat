df30a198f28d1f03db2d9267b4c56b1e80e19cb0
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-12 19:52:07 +0100

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,86 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem # Copyright 2015-2022 the original author or authors.
+@rem #
+@rem # Licensed under the Apache License, Version 2.0 (the "License");
+@rem # you may not use this file except in compliance with the License.
+@rem # You may obtain a copy of the License at
+@rem #
+@rem #      https://www.apache.org/licenses/LICENSE-2.0
+@rem #
+@rem # Unless required by applicable law or agreed to in writing, software
+@rem # distributed under the License is distributed on an "AS IS" BASIS,
+@rem # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem # See the License for the specific language governing permissions and
+@rem # limitations under the License.
+@rem ##########################################################################
+
+@rem ##########################################################################
+@rem #
+@rem # Gradle startup script for MS Windows
+@rem #
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it absolute
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fpi
+
+@rem Project-specific JVM options can be set in an environment variable or properties file
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+set DIRNAME=
+if "%OS%"=="Windows_NT" set "DIRNAME=%USERPROFILE%\.gradle"
+
+@rem Find java.exe
+if not "%JAVA_HOME%" == "" goto FindJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_HOME_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:FindJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
