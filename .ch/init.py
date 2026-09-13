1d6153d6040f60a885870935820b3bfcbcb1ea05
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-13 19:57:49 +0100

    Remove unnecessary blank lines in init.py

--- .ch/init.py
+++ .ch/init.py
@@ -25,8 +25,6 @@ from scipy.signal import find_peaks
 from scipy.signal import peak_widths
 from scipy.signal import savgol_filter
 
-
-
 def load_challenge_data(filename):
     x = loadmat(filename)
     data = np.asarray(x['val'], dtype=np.float64)
